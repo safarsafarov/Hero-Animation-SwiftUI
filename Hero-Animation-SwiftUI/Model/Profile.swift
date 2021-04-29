65418223385d89d07eb7c049e05d0b70d2c4471a
safarsafarov <safarov.safar@hotmail.com>
2021-04-29 11:15:11 +0800

    commit

--- Hero-Animation-SwiftUI/Model/Profile.swift
+++ Hero-Animation-SwiftUI/Model/Profile.swift
@@ -7,4 +7,22 @@
 
 import SwiftUI
 
-// 
+// Profile Mode and Sample Recents Msgs Lists...
+
+struct Profile: Identifiable {
+    var id = UUID().uuidString
+    var userName: String
+    var profile: String
+    var lastMsg: String
+    var time: String
+}
+
+let recents = [
+    Profile(id: "iJustine", userName: "pic1", profile: "Hi What'sup", lastMsg: "Fuck off", time: "10:12"),
+    Profile(id: "iJustine", userName: "pic1", profile: "Hi What'sup", lastMsg: "Fuck off", time: "10:12"),
+    Profile(id: "iJustine", userName: "pic1", profile: "Hi What'sup", lastMsg: "Fuck off", time: "10:12"),
+    Profile(id: "iJustine", userName: "pic1", profile: "Hi What'sup", lastMsg: "Fuck off", time: "10:12"),
+    Profile(id: "iJustine", userName: "pic1", profile: "Hi What'sup", lastMsg: "Fuck off", time: "10:12"),
+    Profile(id: "iJustine", userName: "pic1", profile: "Hi What'sup", lastMsg: "Fuck off", time: "10:12"),
+    Profile(id: "iJustine", userName: "pic1", profile: "Hi What'sup", lastMsg: "Fuck off", time: "10:12")
+]
