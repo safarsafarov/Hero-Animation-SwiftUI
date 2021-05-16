609c3ba25cfa94b4e6625394a575369310375483
safarsafarov <safarov.safar@hotmail.com>
2021-05-17 03:08:05 +0800

    commit

--- Hero-Animation-SwiftUI/Model/ProfileDetailView.swift
+++ Hero-Animation-SwiftUI/Model/ProfileDetailView.swift
@@ -8,42 +8,74 @@
 import SwiftUI
 
 struct ProfileDetailView: View {
+    
     var animation: Namespace.ID
     @EnvironmentObject var profileData: ProfileDetailModel
     @GestureState var offset: CGFloat = 0
+    
     var body: some View {
-        if profileData.selectedProfile != nil{
-            Image(profileData.selectedProfile.profile)
-                .resizable()
-                .aspectRatio(contentMode: .fit)
-                .overlay(BottomActions(), alignment: .bottom)
-                .matchedGeometryEffect(id: profileData.selectedProfile.id, in: animation)
-                .frame(width: 300, height: 300)
-                //Background Color...
-                .frame(maxWidth: .infinity, maxHeight: .infinity)
-                .background(
-                    
-                    Color("bg")
-                        .opacity(0.2)
-                        .ignoresSafeArea()
-                        // closing when tapping on background...
-                        .onTapGesture {
+        
+        ZStack(alignment: .top) {
+            
+            if profileData.selectedProfile != nil{
+                
+                if profileData.showEnglargedImage {
+                    Image(profileData.selectedProfile.profile)
+                        .resizable()
+                        .aspectRatio(contentMode: .fit)
+                        .matchedGeometryEffect(id: profileData.selectedProfile.id, in: animation)
+                        .frame(width: .infinity, height: .infinity)
+                        //Background Color...
+                        .offset(y: $profileData.offset)
+                        .gesture(DragGesture().updating($offset, body: { value, out, _) in
+                            out = value.translation.height
+                        }).onEnded({ (value) in
+                            let offset = profileData.offset > 0 ? profileData.offset: -profileData.offset
                             withAnimation{
+                                if offset > 200{
+                                    profileData.showProfile.toggle()
+                                    profileData.showEnlargedImage.toggle()
+                                    profileData.selectedProfile = nil
+                                }
+                                profileData.offset = 0
+                            }
+                            
+                        }))
+                        .background(Color("bg").opacity(getOpacity()).ignoreSafeArea())
+                    
+                    HStack(spacing: 20){
+                        
+                        Button(action: {
+                            
+                            withAnimation{
+                                profileData.showEnlargedImage.toggle()
                                 profileData.showProfile.toggle()
                                 profileData.selectedProfile = nil
                             }
-                        }
-                )
+                        }, label: {
+                            Image(systemName: "arrow.left")
+                                .font(.title2)
+                                .foregroundColor(.white)
+                        })
+                        
+                        Text(profileData.selectedProfile.userName)
+                            .font(.title2)
+                            .fontWeight(.semibold)
+                            .foregroundColor(.white)
+                            .matchedGeometryEffect(id: "TEXT_(profileData.selectedProfile.id", in: animation)
+                    }
+                }
+            }
         }
     }
 }
+
 
 struct ProfileDetailView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
     }
 }
-
 
 // Bottom Actions...
 
@@ -67,3 +99,4 @@ struct BottomActions: View {
         .background(Color.white)
     }
 }
+
